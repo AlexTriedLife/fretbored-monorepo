54c73e5b6bf510d71d4b93d688833bd2d2beaae6
AlexTriedLife <alexmcphersonuribe@gmail.com>
2026-05-30 16:57:31 -0400

    feat(theory): generate major and natural minor scales based off root note

--- core_engine/src/theory/scale.rs
+++ core_engine/src/theory/scale.rs
@@ -1,1 +1,133 @@
+use crate::theory::Note;
+use crate::theory::interval::{Interval, calculate_target_note};
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// Major and minor for now
+// TODO: Add modes and different scales
+pub enum ScaleType {
+    Major,
+    NaturalMinor,
+}
+
+impl ScaleType {
+    // Returns a static slice containing the intervals of a scale relative to the root note
+    pub fn intervals(&self) -> &'static [Interval] {
+        match self {
+            ScaleType::Major => &[
+                Interval::MajorSecond,
+                Interval::MajorThird,
+                Interval::PerfectFourth,
+                Interval::PerfectFifth,
+                Interval::MajorSixth,
+                Interval::MajorSeventh,
+            ],
+            ScaleType::NaturalMinor => &[
+                Interval::MajorSecond,
+                Interval::MinorThird,
+                Interval::PerfectFourth,
+                Interval::PerfectFifth,
+                Interval::MinorSixth,
+                Interval::MinorSeventh,
+            ],
+        }
+    }
+}
+
+// Returns generated scale as a vector of note enums
+pub fn generate_scale(root: Note, scale_type: ScaleType) -> Vec<Note> {
+    // Retrieve the interval/step pattern of the scale type
+    let intervals = scale_type.intervals();
+
+    // Dynamically allocate capacity (rootnote + number of intervals in scale)
+    let mut scale = Vec::with_capacity(intervals.len() + 1);
+
+    // Add the root note to beginning of scale
+    scale.push(root);
+
+    // Calculate following notes and add to scale
+    // Deref interval
+    for &interval in intervals {
+        scale.push(calculate_target_note(root, interval));
+    }
+
+    scale
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::theory::Note;
+
+    #[test]
+    fn test_generate_major_scale() {
+        // Generate a C Major scale
+        let c_major = generate_scale(Note::C, ScaleType::Major);
+
+        // Expected: C, D, E, F, G, A, B
+        assert_eq!(
+            c_major,
+            vec![
+                Note::C,
+                Note::D,
+                Note::E,
+                Note::F,
+                Note::G,
+                Note::A,
+                Note::B,
+            ]
+        );
+
+        // Generate a A#/Bb Major scale
+        let asharp_major = generate_scale(Note::ASharp, ScaleType::Major);
+
+        // Expected: A#, C, D, D#, F, G, A
+        assert_eq!(
+            asharp_major,
+            vec![
+                Note::ASharp,
+                Note::C,
+                Note::D,
+                Note::DSharp,
+                Note::F,
+                Note::G,
+                Note::A,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_generate_natural_minor_scale() {
+        // Generate an A Natural Minor scale
+        let a_minor = generate_scale(Note::A, ScaleType::NaturalMinor);
+
+        // Expected: A, B, C, D, E, F, G
+        assert_eq!(
+            a_minor,
+            vec![
+                Note::A,
+                Note::B,
+                Note::C,
+                Note::D,
+                Note::E,
+                Note::F,
+                Note::G,
+            ]
+        );
+
+        // Generate a C# Natural Minor scale
+        let csharp_minor = generate_scale(Note::CSharp, ScaleType::NaturalMinor);
+
+        // Expected: C#, D#, E, F#, G#, A, B
+        assert_eq!(
+            csharp_minor,
+            vec![
+                Note::CSharp,
+                Note::DSharp,
+                Note::E,
+                Note::FSharp,
+                Note::GSharp,
+                Note::A,
+                Note::B,
+            ]
+        );
+    }
+}
